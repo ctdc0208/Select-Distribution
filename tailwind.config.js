c109c838dd8b4ee6f5a5ff1717e1ce95987fc189
Christian dela Cruz <66873179+ctdc0208@users.noreply.github.com>
2024-02-05 23:23:02 +0800

    add border

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,13 @@ module.exports = {
     extend: {
       colors: {
         'red': '#b42319',
+        'gray': '#ebebeb',
       },
       fontSize: {
         'small': '0.5rem'
+      },
+      boxShadow: {
+        'solid': '0px 11px 2px 5px rgba(235,235,235,1)',
       }
     },
   },
